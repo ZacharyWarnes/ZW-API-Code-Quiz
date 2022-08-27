1f8a043e302df08c849f921707ee4cc1f63b2ad1
Zachary Warnes <zacharywarnes@gmail.com>
2022-08-26 17:35:39 -0700

    adding pseudocode for js. Adding some elements on HTML and their link in js

---
+++ assets/js/game.js
@@ -0,0 +1,49 @@
+// DECLARE elements for HTML
+var timerEL = document.getElementById('timer');
+var startButtonEl= docuemnt.getElementById('startbutton');
+
+
+// DECLARE countdown in number
+
+// DECLARE questions by array list of objects
+
+var questions = [
+    {
+        // question:
+        // answer:
+        // correct answer:
+    },
+        
+    {
+        //question
+        //answer
+        //correct answer
+    },
+        
+    {
+        //question
+        //answer
+        //correct answer
+    },
+];
+
+//function for starting the game 
+
+    //hide start screen
+
+    //display the first question
+
+    //set the starting value of 'countdown'
+
+    //start the timer
+
+//Function for selecting and answer
+
+    // If the answer is wrong 
+        
+        //THEN we need to subtract from 'countdown' 
+
+//display the next question
+
+
+//Display Current question 
