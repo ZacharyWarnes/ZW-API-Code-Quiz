fa515b8c107837f75e3c39e85b09e988d920e947
Zachary Warnes <zacharywarnes@gmail.com>
2022-08-27 14:27:21 -0700

    starting function to record high score by user initial on viewHighScore.js

--- assets/js/viewHighScore.js
+++ assets/js/viewHighScore.js
@@ -1,3 +1,16 @@
 var highScoresEl = document.getElementById('highScores');
 var highScores = [];
 var highScoreInitials = [];
+
+//This function will record the users high score and add to local storage
+function recordHighScore(userScore) {
+    var initials = document.getElementById("scorerInitials").value;
+//This bit sets the entered values to strings and then pushes them to and empty array for scores and user initials to be used in the highscores page
+    localStorage.setItem("userScore",JSON.stringify(userScore));
+    localStorage.setItem("initials", JSON.stringify(initials));
+    highScores.push(userScore);
+    highScoreInitials.push(initials);
+
+
+
+}
