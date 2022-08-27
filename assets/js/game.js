03fc48643a2fde7ae79a87c82ecfc448211378ef
Zachary Warnes <zacharywarnes@gmail.com>
2022-08-27 00:28:11 -0700

    building question array, building template literal for displayed questions, targeting elements to be inserted into the template literal

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,32 +2,15 @@
 var timerEL = document.getElementById('timer');
 var startButtonEl= docuemnt.getElementById('startbutton');
 var questionPosition= 0;
-var currentQuestion = questions[questionPosition];
-var timeLeft = 0;
+var currentQuestion = questionArray[questionPosition];
+var timeInterval;
+
 
 // DECLARE countdown in number
 
 // DECLARE questions by array list of objects
 
-var questions = [
-    {
-        // question:
-        // answer:
-        // correct answer:
-    },
-        
-    {
-        //question
-        //answer
-        //correct answer
-    },
-        
-    {
-        //question
-        //answer
-        //correct answer
-    },
-];
+
 
 //function 'startGame' 
 
@@ -36,12 +19,26 @@ var questions = [
 
     //display the first question
 
-    //set the starting value of 'countdown'
+// `<h2>//Question</h2>
+//             <h3 id="showQuestion">Test</h3>
+//         </div>
+//         <div class="answers">
+//             <h2>//Answers</h2>
+//             <button class="buttons" id="answer1" data-answer = "0"></button>
+//             <button class="buttons" id="answer2" data-answer = "1"></button>
+//             <button class="buttons" id="answer3"></button>
+//             <button class="buttons" id="answer4"></button>
+//         </div>
+//     </div>   
+//     <div class="timer">
+//         <h2>//Timer</h2>
+//         <p id="countdown"></p>
+//     </div>`
 
     //start the timer
 function countdown() {
     var timeLeft = 75;
-    var timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
         timeLeft --;
         timerEL.textContent = timeLeft;
         if (timeLeft === 0) {
@@ -51,8 +48,53 @@ function countdown() {
     })
 
 } 
-        //IF countdown === 0 THEN endGame()
+function endGame() {
+    clearInterval(timeInterval);
+}
 
+//An array of questions for the quiz game
+var questionArray = [
+    {
+        id: 1,
+        question: "What data type has the value of true or false in javascript?",
+        answer: ["String", "Number", "Boolean", "Pizza"],
+        correctAnswer: "Boolean" 
+    },
+        
+    {
+        id: 2,
+        question: "The contents of a function should be wrapped in what?",
+        answer: ["parenthasis", "Square brackets", "Pizza", "Curly brackets"],
+        correctAnswer: "Curly brackets"
+    },
+        
+    {
+        //question
+        //answer
+        //correct answer
+    },
+];
+
+function showQuestions (questionPosition) {
+
+    //Creating Elements for template literal
+    var questionEl = document.getElementById("showQuestion");
+    var firstAnswerEl = docuemnt.getElementById("answer1");
+    var secondAnswerEl = document.getElementById("answer2");
+    var thirdAnswerEL = document.getElementById("answer3");
+    var fourthAnswerEl = document.getElementById("answer4");
+
+    //Show the question for template literal
+    questionEl.textContent = questionArray[questionPosition].question;
+
+    //Show answer options for template literal
+    firstAnswerEl.textContent = "1. " + questionArray[questionPosition].answers[0];
+    secondAnswerEl.textContent = "2. " + questionArray[questionPosition].answers[1];
+    thirdAnswerEL.textContent = "3. " + questionArray[questionPosition].answer[2];
+    fourthAnswerEl.textContent = "4. " + questionArray[questionPosition].answer[3];
+
+
+}
 //Function 'answerQuestion'
 
     // If the answer is wrong 
@@ -65,3 +107,5 @@ function countdown() {
 
 //Display Current question 
 // function displayCurrentQuestion
+
+//function recordHighScore
